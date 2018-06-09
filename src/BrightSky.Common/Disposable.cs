fd95ea36160c90ab52b86d1a49f16d6872e64755
Mahmood Akhtar <mahmood233@hotmail.com>
2018-06-09 17:46:10 +0100

    Refactored method signature for Disposable.Using method to be inline with naming conventions eslewhere.

--- src/BrightSky.Common/Disposable.cs
+++ src/BrightSky.Common/Disposable.cs
@@ -4,7 +4,7 @@ namespace BrightSky.Common
 {
     public static class Disposable
     {
-        public static TResult Using<TDisposable, TResult>(Func<TDisposable> factory, Func<TDisposable, TResult> function) where TDisposable : IDisposable
+        public static TOutput Using<TInput, TOutput>(Func<TInput> factory, Func<TInput, TOutput> function) where TInput : IDisposable
         {
             using (var disposable = factory())
             {
