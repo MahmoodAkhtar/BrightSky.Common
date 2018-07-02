18838f32ea688ab3fb7c9ef0d84e4c253fd7c6fa
Mahmood Akhtar <mahmood233@hotmail.com>
2018-07-02 12:15:52 +0100

    Added another variation of Handling.TryCatch where the catchFunc is a delefate that returns TOutput.

--- src/BrightSky.Common/Handling.cs
+++ src/BrightSky.Common/Handling.cs
@@ -4,6 +4,18 @@ namespace BrightSky.Common
 {
     public static class Handling
     {
+        public static TOutput TryCatch<TException, TOutput>(Func<TOutput> factory, Func<TException, TOutput> catchFunc) where TException : Exception
+        {
+            try
+            {
+                return factory();
+            }
+            catch (TException ex)
+            {
+                return catchFunc(ex);
+            }
+        }
+
         public static TOutput TryCatch<TException, TOutput>(Func<TOutput> factory, Action<TException> catchAction) where TException : Exception
         {
             TOutput output = default(TOutput);
